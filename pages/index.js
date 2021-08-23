95067ffeed36455809c7487ca05405ade1ac2362
J.P. Zambrano <juan.p.zambrano@raytheon.com>
2021-08-23 10:26:04 -0500

    Removes template code

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
-      Hello World.{' '}
-      <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
+      {/* <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
         <a>About</a>
-      </Link>
+      </Link> */}
     </div>
   )
 }
