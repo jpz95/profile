ada454bfc5c9f5188e82e7b4bcb6498fe7f75eb3
J.P. Zambrano <juan.p.zambrano@raytheon.com>
2021-09-06 17:35:45 -0500

    Adds typewriter styles using blue/gold colors

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import useTypeWriter from 'hooks/useTypeWriter.ts'
+import useTypeWriter from 'hooks/useTypeWriter.ts';
+
+import styles from './Home.module.scss';
 
 export default function Home() {
   const sentences = [
@@ -8,7 +10,7 @@ export default function Home() {
   const text = useTypeWriter(sentences);
   return (
     <div>
-      <h2>{text}</h2>
+        <h2 className={styles.typeWriter}>{text}</h2>
       {/* <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
         <a>About</a>
       </Link> */}
