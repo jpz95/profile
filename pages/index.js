22d883b3629a9596d0df2829778995aef247b57f
J.P. Zambrano <juan.p.zambrano@raytheon.com>
2021-09-06 18:59:14 -0500

    Creates console window as home page

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,19 @@ import styles from './Home.module.scss';
 export default function Home() {
   const sentences = [
     { text: 'Hello there!', transitionDelay: 1500 }, // set to good morning/afternoon/evening
-    { text: `I'm J.P., a detail-oriented frontend engineer`, transitionDelay: 2500 },
+    { text: `I'm J.P., and I'm fluent in code c:`, transitionDelay: 2000 },
   ];
   const text = useTypeWriter(sentences);
   return (
-    <div>
-        <h2 className={styles.typeWriter}>{text}</h2>
+    <div className={styles.home}>
+      <div className={styles.window}>
+        <div className={styles.console}>
+          <h2 className={styles.typeWriter}>
+            {text}
+            <span className={styles.textCursor} />
+          </h2>
+        </div>
+      </div>
       {/* <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
         <a>About</a>
       </Link> */}
