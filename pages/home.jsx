7bfc422e4b4e1f4a01e0d688ee4c0a7facffbbdc
J.P. Zambrano <jpzambrano95@hotmail.com>
2021-09-16 09:56:32 -0500

    Creates empty home page

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,14 +1,12 @@
-import Link from 'next/link';
+import styles from './Home.module.scss';
 
 export default function Home() {
   return (
     <div>
-      <div>About us</div>
-      <div>
-        Back to{' '}
-        <Link href="/" as={process.env.BACKEND_URL + '/'}>
-          <a>Intro</a>
-        </Link>
+      <div className={styles.window}>
+        <div className={styles.screen}>
+          stuff
+        </div>
       </div>
     </div>
   );
