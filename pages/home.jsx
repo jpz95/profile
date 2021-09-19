0f82111537c0e16da5fb17f709394c4c9cc0b7fd
J.P. Zambrano <jpzambrano95@hotmail.com>
2021-09-19 18:18:31 -0500

    Uses device screen component for home page

--- pages/home.jsx
+++ pages/home.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import useHref from 'hooks/useHref.ts';
 
 import styles from './Home.module.scss';
+import DeviceScreen from 'components/DeviceScreen';
 
 export default function Home() {
   const desktopApps = [
@@ -14,32 +15,28 @@ export default function Home() {
   ];
 
   return (
-    <div>
-      <div className={styles.window}>
-        <div className={styles.screen}>
-          <main className={styles.desktop}>
-            <div className={styles.banner}>
-              {/* TODO: attach some welcome message with a nice background (carousel?) */}
-              <div className={styles.bannerImg}>some img</div>
-            </div>
-            <div className={styles.desktopAppContainer}>
-              {desktopApps.map(app => (
-                <Link
-                  key={app.name}
-                  href={app.link.href}
-                  as={app.link.hrefOnHost}
-                  passHref
-                  >
-                  <div className={styles.desktopApp}>
-                    <span className={styles.appIcon}></span>
-                    <span className={styles.appName}>{app.name}</span>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          </main>
+    <DeviceScreen>
+      <main className={styles.desktop}>
+        <div className={styles.banner}>
+          {/* TODO: attach some welcome message with a nice background (carousel?) */}
+          <div className={styles.bannerImg}>some img</div>
         </div>
-      </div>
-    </div>
+        <div className={styles.desktopAppContainer}>
+          {desktopApps.map(app => (
+            <Link
+              key={app.name}
+              href={app.link.href}
+              as={app.link.hrefOnHost}
+              passHref
+              >
+              <div className={styles.desktopApp}>
+                <span className={styles.appIcon}></span>
+                <span className={styles.appName}>{app.name}</span>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </DeviceScreen>
   );
 }
